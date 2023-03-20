cc2d437f940f22e966ec2cff783b8b86a6a09740
vagrant <vagrant@master.puppet.vm>
2023-03-20 16:55:46 +0000

    content change

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,12 @@
 node default {
   file {'/root/README':
-    ensure => file
+    ensure  => file,
+    content => 'This is a README\n',
+    owner   => 'root'
   }
+
+# No duplicates allowed
+  # file {'/root/README':
+  #   owner   => 'root'
+  # }
 }
